fc04468e81376ee236f26f05727d3cf49472cc6b
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2022-12-28 15:06:03 +0530

    Add files via upload

---
+++ practice_code.py
@@ -0,0 +1,34 @@
+print("Date is:",23)
+
+
+#if 100>99 and\
+#200<=300 and\
+#True!=False:
+  #  print("Hello world!")
+l=[1,34,5,76,
+   23,87,3,77,
+   56,12,17,43]
+print(l)
+print()
+print()
+def add(a,b):
+    t=a+b
+    print(t)
+
+print("Hello world")
+add(38,179)
+
+print("Hello world \n welcome to Bangalore")
+
+
+print("Aprameya","","Aithal",end="-")
+print("Euro School")
+print()#adds a line
+print()#adds another line
+a=5
+if a>5:
+    print("a is greater than 5")
+else: 
+    print("a is less than or equal to 5")
+
+print("Python is a widely used general purpose,high level programmimg language.There are two major versions of python, python 2 and python3")
