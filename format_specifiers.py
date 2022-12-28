c357d5e1e248407e2ee58f4d39fc1cc038449af9
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2022-12-28 15:07:07 +0530

    Add files via upload

---
+++ format_specifiers.py
@@ -0,0 +1,32 @@
+#name="Anu"
+#age=25
+#print("Hi %s your age is %i"%(name,age))
+
+#a=15
+#b=25
+#print("value of a is %d and value of b is %d"%(a,b))
+
+
+name="Ram"
+age=25
+city= "Bengaluru"
+print(name,age)
+print("Name is:", name)
+print("Age is:",age)
+print()
+print()
+print("Hi {} your age is {}".format(name,age))
+#print("your age is {},{}".format(age,name))
+print("Your age is {1},{0}".format(name,age))
+print()
+print("Hi {name} welcome for {course}".format(name="Mary",course="Python"))
+print()
+print(f"Welcome {name} to {city}")
+
+print()
+print(f"Hello,my name is {name} and my age is {age} and I am from {city}")
+a=45
+b=76
+print(f"Addition of {a} and {b} is {a+b}")
+print()
+print(f"Datatype of a is {type(a)}")
