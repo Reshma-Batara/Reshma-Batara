162129dd837592a860f44fb9f69cc9065d922329
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2022-12-28 15:02:14 +0530

    Add files via upload

---
+++ First_code.py
@@ -0,0 +1,23 @@
+print("Hello world")
+print("Good morning")
+a=5
+b=36
+c=a+b
+print("Sum is :",c)
+x=34
+y=23
+z=x*y
+print("Product is :",z)
+student_name="Ram"
+age=25
+print(student_name, age)
+print("Address of student_name:",id(student_name))
+print("Address of age:",id(age))
+print(type(student_name))
+print(type(age))
+print(student_name, age)
+print(type(True))
+print()
+print("Hello world")
+print(student_name,age,sep="*",end="-")
+print("hello world")
